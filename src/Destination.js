dfe0f526b0c2752d8b9c746e362338a58a9b7acb
Farhan <farhanmnavas@gmail.com>
2022-10-25 16:23:03 +0800

    Complete destinations page content

--- src/Destination.js
+++ src/Destination.js
@@ -4,30 +4,53 @@ import Data from './starter-code/data.json'
 export default class Destination extends Component {
     constructor(props) {
         super(props)
-        this.state = { destination: (0) }
-        this.getDestination = this.getDestination.bind(this)
+        this.state = { destination: 0 }
+        this.handleClick = this.handleClick.bind(this)
     }
 
-    getDestination() {
-        const destData = Data.destinations[this.state.destination]
-        const destName = destData.name
-        const destDesc = destData.description
-        const destImage = './starter-code' + destData.images.substring(1)
-        console.log(destImage)
-        return (
-            <>
-                <h1 className='name'>{ destName }</h1>
-                <p className='description'>{ destDesc }</p>
-                <figure className='image'>
-                    <img src={destImage} alt={'Image of the' + destName} />
-                </figure>
-            </>
-        )
+    handleClick(e) {
+        if (e.target.className === 'Moon') {
+            this.setState({ destination: 0 })
+        } else if (e.target.className === 'Mars') {
+            this.setState({ destination: 1 })
+        } else if (e.target.className === "Europa") {
+            this.setState({ destination: 2 })
+        } else if (e.target.className === "Titan") {
+            this.setState({ destination: 3 })
+        }
     }
 
     render() {
+        const destData = Data.destinations[this.state.destination]
+        const destName = destData.name
+        const destDesc = destData.description
+        const destImage = destData.images.substring(1)
+        const destDistance = destData.distance
         return (
-            this.getDestination()
+            <div className='Destination'>
+                <h1 className='Destination-heading'>01 PICK YOUR DESTINATION</h1>
+                <figure className='Destination-image'>
+                    <img src={require(`./starter-code${destImage}`)} alt={`Image of the ${destName}`} />
+                </figure>
+                <section>
+                    <button onClick={this.handleClick} className='Moon'>Moon</button>
+                    <button onClick={this.handleClick} className='Mars'>Mars</button>
+                    <button onClick={this.handleClick} className='Europa'>Europa</button>
+                    <button onClick={this.handleClick} className='Titan'>Titan</button>
+                </section>
+                <h1 className='Destination-name'>{ destName }</h1>
+                <p className='Destination-desc'>{ destDesc }</p>
+                <div>
+                    <div className='Destination-distance'>
+                        <h3>AVG. DISTANCE</h3>
+                        <h2>{ destDistance }</h2>
+                    </div>
+                    <div className='Destination-distance'>
+                        <h3>AVG. DISTANCE</h3>
+                        <h2>{ destDistance }</h2>
+                    </div>
+                    </div>              
+            </div>
         )
     }
 }
