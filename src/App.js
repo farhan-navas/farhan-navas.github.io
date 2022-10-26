60ef53f9cb5885e416b0e490aa605ffb1d245870
Farhan <farhanmnavas@gmail.com>
2022-10-26 14:28:22 +0800

    Add active and position navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link, NavLink } from 'react-router-dom';
 import Crew from './Crew';
 import Destination from './Destination';
 import Home from './Home'
-import NotFound from './NotFound';
 import Technology from './Technology';
 import './App.css'
+import Logo from './starter-code/assets/shared/logo.svg'
 
 function App() {
   return (
     <main className='App'>
       <nav className='NavBar'>
-        <h6><Link to={'/'}>00 HOME</Link></h6>
-        <h6><Link to={'/destination'}>01 DESTINATION</Link></h6>
-        <h6><Link to={'/crew'}>02 CREW</Link></h6>
-        <h6><Link to={'/technology'}>03 TECHNOLOGY</Link></h6>
+        <img src={Logo} />
+        <h6><NavLink to={'/home'} className={({isActive}) => isActive ? 'active': undefined}>00 HOME</NavLink></h6>
+        <h6><NavLink to={'/destination'} className={({isActive}) => isActive ? 'active': undefined}>01 DESTINATION</NavLink></h6>
+        <h6><NavLink to={'/crew'} className={({isActive}) => isActive ? 'active': undefined}>02 CREW</NavLink></h6>
+        <h6><NavLink to={'/technology'} className={({isActive}) => isActive ? 'active': undefined}>03 TECHNOLOGY</NavLink></h6>
       </nav>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/destination' element={<Destination />} />
         <Route path='/crew' element={<Crew />} />
         <Route path='/technology' element={<Technology />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </main>
   );
