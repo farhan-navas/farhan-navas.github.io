a6dff7a4c458c30dba8ce00a24801226b519588b
Farhan <farhanmnavas@gmail.com>
2022-10-26 17:19:04 +0800

    Complete NavBar component

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ function App() {
   return (
     <main className='App'>
       <nav className='NavBar'>
-        <img src={Logo} />
-        <h6><NavLink to={'/home'} className={({isActive}) => isActive ? 'active': undefined}>00 HOME</NavLink></h6>
-        <h6><NavLink to={'/destination'} className={({isActive}) => isActive ? 'active': undefined}>01 DESTINATION</NavLink></h6>
-        <h6><NavLink to={'/crew'} className={({isActive}) => isActive ? 'active': undefined}>02 CREW</NavLink></h6>
-        <h6><NavLink to={'/technology'} className={({isActive}) => isActive ? 'active': undefined}>03 TECHNOLOGY</NavLink></h6>
+        <img src={Logo} alt={Logo} />
+        <hr></hr>
+        <div className='links'>
+          <h6><NavLink to={'/home'} className={({isActive}) => isActive ? 'active': undefined}><span>00</span> HOME</NavLink></h6>
+          <h6><NavLink to={'/destination'} className={({isActive}) => isActive ? 'active': undefined}><span>01</span> DESTINATION</NavLink></h6>
+          <h6><NavLink to={'/crew'} className={({isActive}) => isActive ? 'active': undefined}><span>02</span> CREW</NavLink></h6>
+          <h6><NavLink to={'/technology'} className={({isActive}) => isActive ? 'active': undefined}><span>03</span> TECHNOLOGY</NavLink></h6>
+        </div>
       </nav>
       <Routes>
         <Route path='/home' element={<Home />} />
