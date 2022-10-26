800bd4bcd4dc0cf9c3c4b50a4c3a62d4adb39a6c
Farhan <farhanmnavas@gmail.com>
2022-10-26 21:57:10 +0800

    Complete styling for Home Page, except Explore button hover

--- src/Home.js
+++ src/Home.js
@@ -6,17 +6,16 @@ export default class Home extends Component {
         return (
             <div className='Home'>
                 <main className='MainPage'>
-                    <h4 className='MainPage-subheading'>So, You want to travel to space</h4>
+                    <h4 className='MainPage-subheading'>So, You want to travel to</h4>
                     <h1 className='MainPage-heading'>SPACE</h1>
-                </main>
-
-                <div className='MainPage-content'>
+                    <div className='MainPage-content'>
                     Let's face it: if you want to go to space, you might as well genuinely go to outer space and not kind of on the edge of
                     it. Well sit back, and relax because we'll give you a truly out of this world experience.
-                </div>
+                    </div>
+                </main>
 
                 <div className='MainPage-button'>
-                    <button>EXPLORE</button>
+                    <h3>EXPLORE</h3>
                 </div>
             </div>            
         )
